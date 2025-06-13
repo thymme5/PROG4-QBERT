7aab8d970fe15fe314f5025fbdda3889880a5232
Thymme-Mouchrique <Thymme5@outlook.com>
2025-06-13 14:33:32 +0200

    Implemented proper high score tracking

--- GameTest/EnterGameMode.h
+++ GameTest/EnterGameMode.h
@@ -10,17 +10,21 @@ class EnterGameMode final : public dae::Command
 {
 public:
     explicit EnterGameMode(MainMenuUIComponent* menuUI)
-        : m_pMenuUI(menuUI)
+        : m_pMenuUI(menuUI), m_UseIndexFromMenu(true)
     {
+    }
 
+    explicit EnterGameMode(int index)
+        : m_Selected(index), m_UseIndexFromMenu(false)
+    {
     }
 
     void Execute() override
     {
-        if (!m_pMenuUI)
-            return;
+        int selected = m_UseIndexFromMenu && m_pMenuUI
+            ? m_pMenuUI->GetSelectedIndex()
+            : m_Selected;
 
-        const int selected = m_pMenuUI->GetSelectedIndex();
         switch (selected)
         {
         case 0:
@@ -33,7 +37,10 @@ public:
             GameModeManager::GetInstance().SetMode(std::make_unique<VersusMode>(1));
             break;
         case 3:
-            GameModeManager::GetInstance().SetMode(std::make_unique<HighScoreMenu>( ));
+            GameModeManager::GetInstance().SetMode(std::make_unique<HighScoreMenu>());
+            break;
+        case 4:
+            GameModeManager::GetInstance().SetMode(std::make_unique<MainMenu>());
             break;
         default:
             break;
@@ -41,5 +48,7 @@ public:
     }
 
 private:
-    MainMenuUIComponent* m_pMenuUI;
+    MainMenuUIComponent* m_pMenuUI{};
+    int m_Selected{};
+    bool m_UseIndexFromMenu{ true };
 };
